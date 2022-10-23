e5d250adc99de1edce485a5ed6300f9f3c478838
Kirill Tchentsov <kirill.develops@gmail.com>
2022-10-23 16:05:02 -0400

    refactored BackButton styles

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Button } from 'gatsby-theme-material-ui';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { darkTheme, lightTheme } from '../styles/theme';
 
-function BackButton({ isDark }) {
+const backButtonStyleProps = {
+  my: { xs: 4, md: 'auto' },
+  ml: 4.5,
+  width: '125px',
+  fontWeight: 600,
+  boxShadow:
+    '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
+};
+
+function BackButton() {
   return (
     <Button
       variant="text"
       to="/"
       startIcon={<ArrowBackIcon />}
-      sx={{
-        my: { xs: 4, md: 'auto' },
-        ml: 4.5,
-        width: '125px',
-        backgroundColor: isDark
-          ? darkTheme.palette.background.paper
-          : lightTheme.palette.background.paper,
-        fontWeight: 600,
-        boxShadow:
-          '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
-      }}
+      sx={backButtonStyleProps}
     >
       Back
     </Button>
