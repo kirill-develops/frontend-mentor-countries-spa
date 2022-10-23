752ae4d11c09cbe3e61dab394a6fbc93c5492e9d
Kirill Tchentsov <kirill.develops@gmail.com>
2022-10-23 01:35:04 -0400

    refactored BackButton to have appropriate background color

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Button } from 'gatsby-theme-material-ui';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { darkTheme, lightTheme } from '../styles/theme';
 
-function BackButton() {
+function BackButton({ isDark }) {
   return (
     <Button
       variant="text"
@@ -12,6 +13,10 @@ function BackButton() {
         my: { xs: 4, md: 'auto' },
         ml: 4.5,
         width: '125px',
+        backgroundColor: isDark
+          ? darkTheme.palette.background.paper
+          : lightTheme.palette.background.paper,
+        fontWeight: 600,
         boxShadow:
           '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
       }}
