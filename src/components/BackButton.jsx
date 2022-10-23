777f0d5040d63acfa994b92a494a024814d9c3b0
Kirill Tchentsov <kirill.develops@gmail.com>
2022-10-23 00:45:53 -0400

    changed variant & added box shadow to back button

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -5,10 +5,16 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 function BackButton() {
   return (
     <Button
-      variant="outlined"
+      variant="text"
       to="/"
       startIcon={<ArrowBackIcon />}
-      sx={{ my: { xs: 4, md: 'auto' }, ml: 4, width: '25%' }}
+      sx={{
+        my: { xs: 4, md: 'auto' },
+        ml: 4.5,
+        width: '125px',
+        boxShadow:
+          '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
+      }}
     >
       Back
     </Button>
