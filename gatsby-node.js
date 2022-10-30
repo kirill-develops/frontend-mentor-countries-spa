5399a74ed62a05c6ae9deac8f033149bf716c8c7
Kirill Tchentsov <kirill.develops@gmail.com>
2022-10-30 15:46:21 -0400

    saving manual node injection code for reference

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,34 @@
+// const axios = require('axios');
+
+// const NODE_TYPE = 'CountriesApi'
+
+// manually created nodes in attempt to fix HeadAPI error
+// code left for later reference
+
+// exports.sourceNodes = async ({
+//   actions,
+//   createNodeId,
+//   createContentDigest }) => {
+//   const { createNode } = actions;
+//   const response = await axios("https://restcountries.com/v3.1/all");
+//   const { data = [] } = response;
+
+
+//   data.forEach((node, index) => {
+//     createNode({
+//       ...node,
+//       id: createNodeId(`${NODE_TYPE}-${node.name.common}`),
+//       parent: null,
+//       children: [],
+//       internal: {
+//         type: NODE_TYPE,
+//         content: JSON.stringify(node),
+//         contentDigest: createContentDigest(node)
+//       }
+//     })
+//   })
+// }
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     query {
